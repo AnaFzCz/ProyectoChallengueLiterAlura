e46ffb147ff6580c697b1c2b14d87c1b4711eb28
Ana Fernandez <anafzcz@gmail.com>
2025-06-17 21:50:05 -0400

    Actualizaciones

--- src/main/java/br/com/alura/ProjectoChallengueLiterAlura/dto/AutorResumenDTO.java
+++ src/main/java/br/com/alura/ProjectoChallengueLiterAlura/dto/AutorResumenDTO.java
@@ -1,10 +1,13 @@
 package br.com.alura.ProjectoChallengueLiterAlura.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
 public record AutorResumenDTO(String name,
+                              @JsonAlias("birth_year") Integer birthYear,
+                              @JsonAlias("death_year") Integer deathYear,
                               String titulos) {
 
 }
